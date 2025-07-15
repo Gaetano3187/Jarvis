5e115fad65ab13454755b0a2a405c483d63d88eb
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-07-15 08:07:12 +0200

    Update cene-aperitivi.js

--- pages/cene-aperitivi.js
+++ pages/cene-aperitivi.js
@@ -2,10 +2,10 @@
 import React, { useEffect, useState, useRef } from 'react'
 import Head  from 'next/head'
 import Link  from 'next/link'
-
 import withAuth        from '../hoc/withAuth'
 import { supabase }    from '../lib/supabaseClient'
 import { askAssistant } from '../lib/assistant'
+import { parseAssistant } from '@/lib/assistant';
 
 function CeneAperitivi () {
   /* ------------------- state ------------------- */
