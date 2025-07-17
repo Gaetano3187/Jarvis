634874810844cf4b5cf7e5f960ac1abcf8d6c166
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-07-17 07:16:05 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,10 @@
-// pages/_app.js
-import React from 'react';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import '../styles/globals.css';
-import { AuthProvider } from '../context/AuthContext';
-import NavBar from '../components/NavBar';
+import "../styles/globals.css";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const hideNav = router.pathname === '/';  // barra nascosta nella root
-
   return (
-    <>                                     {/* APERTURA fragment */}
-      <AuthProvider>
-        <Head>
-          <title>Jarvis</title>
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-        </Head>
-
-        {!hideNav && <NavBar />}
-
-        <Component {...pageProps} />
-      </AuthProvider>
-    </>                                     {/* CHIUSURA fragment */}
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
