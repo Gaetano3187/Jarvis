182db0a4f4fc72fe8733eed0bda6f903b2f298f9
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-07-24 09:15:31 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
-import "../styles/globals.css";
-import { AuthProvider } from "../context/AuthContext";
+import '../styles/globals.css';
+import { AuthProvider } from '../context/AuthContext';
+import NavBar from '../components/NavBar';
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Pagine in cui NON vogliamo la NavBar
+  const hideNavOn = ['/', '/login'];   // '/' è index.js (login)
+
+  const showNav = !hideNavOn.includes(router.pathname);
+
   return (
     <AuthProvider>
+      {showNav && <NavBar />}   {/* NavBar visibile ovunque, tranne login */}
       <Component {...pageProps} />
     </AuthProvider>
   );
