020dd5c871da5e1752a8068f526aa97b2e456446
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-07-29 16:31:54 +0000

    fix: rimozione codice non utilizzato in page.js

--- pages/page.js
+++ pages/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-
+import { AuthProvider, AuthContext } from '../context/AuthContext';
 import { useTranslations } from 'next-intl'
 
 const Page = (props) => {
