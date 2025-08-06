478a8f68a96bf0401c646745bdb48f9fe5e60030
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-06 13:39:29 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-/** next.config.js */
 module.exports = {
-  experimental: { wasm: true },
-  webpack(config) {
-    // permette di importare .wasm da node_modules
-    config.resolve.extensions.push('.wasm');
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource'
-    });
-    return config;
+  i18n: {
+    locales: ['en'],
+    defaultLocale: "en",
   }
-};
+}
