45741741c3a4a67ea04a76d8c92e0ebe5ce5879e
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-07 11:25:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // rimuovi entirely la sezione experimental.wasm
   webpack(config) {
-    return config
-  }
-}
-
-
+    // tratta tutti i .wasm come asset/resource
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/wasm/[name].[hash][ext]',
+      },
+    });
+    return config;
+  },
+};
