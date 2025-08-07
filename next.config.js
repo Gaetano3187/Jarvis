c729cf0f10d1faa8d884c58a84a10d70a10b6109
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-08 01:11:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-module.exports = {
-  // rimuovi entirely la sezione experimental.wasm
-  webpack(config) {
-    return config
-  }
+const nextConfig = {
+  reactStrictMode: true,
+  // opzionale se i warning ti bloccano:
+  // eslint: { ignoreDuringBuilds: true },
 }
+module.exports = nextConfig
