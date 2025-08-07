77f3c8ba7befe531d9d90bc889405e30a1759556
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-07 10:09:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
 // next.config.js
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    // gestisci i .wasm come asset/resource, così verranno emessi in /_next/static/
+  webpack(config, { isServer }) {
+    // 1) Copia tesseract-core-simd.wasm in public/
+    if (!isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve(
+                __dirname,
+                'node_modules/tesseract.js-core/tesseract-core-simd.wasm'
+              ),
+              to: path.resolve(__dirname, 'public', 'tesseract-core-simd.wasm'),
+            },
+          ],
+        })
+      )
+    }
+
+    // 2) (Opzionale) Se importi altri .wasm, usarli come asset/resource
     config.module.rules.push({
       test: /\.wasm$/,
       type: 'asset/resource',
       generator: {
-        // dove vengono scritti in output: .next/static/wasm/
         filename: 'static/wasm/[name].[hash][ext]',
-        publicPath: '/_next/',
       },
     })
 
