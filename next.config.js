5b01657243aa1d5a4b83473e600ce1f9fc8d57e9
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-07 09:56:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-module.exports = {
-  // rimuovi entirely la sezione experimental.wasm
-  webpack(config) {
-    return config
-  }
-}
+const nextConfig = {
+  webpack(config, { isServer }) {
+    // permetti di importare .wasm facendo in modo che venga copiato in .next/static/
+    config.module.rules.push({
+      test: /\.wasm$/,
+      // Next >12.0 supporta asset modules, ma per compatibilità usiamo file-loader
+      type: 'javascript/auto',
+      use: [
+        {
+          loader: require.resolve('file-loader'),
+          options: {
+            publicPath: '/_next/static/wasm/',
+            outputPath: 'static/wasm/',
+            name: '[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
