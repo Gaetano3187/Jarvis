fbd13eb68dc795e2d65b812d279b64db41344a92
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-07 21:32:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 module.exports = {
+  // rimuovi entirely la sezione experimental.wasm
   webpack(config) {
-    // abilita il supporto a WebAssembly e lo tratta come asset
-    config.experiments = config.experiments || {};
-    config.experiments.asyncWebAssembly = true;
-
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/wasm/[name].[hash][ext]',
-      },
-    });
-
-    return config;
-  },
-};
+    return config
+  }
+}
