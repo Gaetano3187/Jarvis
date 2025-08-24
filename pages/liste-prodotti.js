8e5ace9a5df3b8452bbf7b230ef617dcabcd3033
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-08-24 10:26:52 +0200

    Change headerBannerVideo width to 100%

--- pages/liste-prodotti.js
+++ pages/liste-prodotti.js
@@ -3407,7 +3407,7 @@ headerBannerBox: {
 },
 
 headerBannerVideo: {
-  width: '80%',
+  width: '100%',
   height: '100%',
   objectFit: 'cover',        // riempie senza bande
   objectPosition: 'center',  // centra soggetti (muletto + scritta)
