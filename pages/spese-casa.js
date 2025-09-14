67b6a4fe8e52fe59f1fed56a9438b6bbbf194c79
Gaetano3187 <gaetano.gargano87@gmail.com>
2025-09-14 17:25:32 +0000

    fes

--- pages/spese-casa.js
+++ pages/spese-casa.js
@@ -86,7 +86,8 @@ function SpeseCasa() {
 
       const { data, error } = await supabase
         .from('jarvis_spese_casa')
-        .select('id, store, name, brand, price_each, price_total, packs, units_per_pack, unit_label, doc_total, receipt_id, purchase_date, created_at')
+       .select('id, store, name, brand, price_each, price_total, packs, units_per_pack, unit_label, doc_total, purchase_date, created_at')
+
         .eq('user_id', user.id)
         .gte('purchase_date', periodStart)
         .lte('purchase_date', periodEnd)
